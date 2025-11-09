cf67ba48862c700560e6b12e87a879bcb19ee565
dmaison <davemaison@gmail.com>
2025-11-09 11:45:57 -0500

    add will

--- birthdays.js
+++ birthdays.js
@@ -1,8 +1,8 @@
-export const BIRTHDAYS = {
+export const BIRTHDAYS = {    
     '3/9': [ 'Joe' ],
+    '5/12': [ 'Will' ],
     '7/19': [ 'Fenste' ],
     '9/17': [ 'Harland' ],
     '10/16': [ 'Dave' ],
-    // '11/9': [ 'Test Birthday' ],
     '11/25': [ 'Bueno' ]
 }
